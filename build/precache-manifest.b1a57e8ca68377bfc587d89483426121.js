dfff714551934c7a182609033c1fd69d3da941a2
Carlos Barata <carlos.barata@timewax.com>
2018-12-06 10:07:37 +0000

    fixed project

---
+++ build/precache-manifest.b1a57e8ca68377bfc587d89483426121.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a589dd374635dca455044ed2465434f",
+    "url": "/static/media/GT-Walsheim-Pro-Regular.9a589dd3.otf"
+  },
+  {
+    "revision": "7495d4b66f8b6d9acfccb5102eced6f8",
+    "url": "/static/media/GT-Walsheim-Pro-Medium.7495d4b6.otf"
+  },
+  {
+    "revision": "1d2540613fb9b9f62865009361a664e6",
+    "url": "/static/media/GT-Walsheim-Pro-Bold.1d254061.otf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "446b2947df248a0129a6",
+    "url": "/static/js/main.446b2947.chunk.js"
+  },
+  {
+    "revision": "0f4da5a68fd197fb2377",
+    "url": "/static/js/1.0f4da5a6.chunk.js"
+  },
+  {
+    "revision": "446b2947df248a0129a6",
+    "url": "/static/css/main.1fb8352f.chunk.css"
+  },
+  {
+    "revision": "0f4da5a68fd197fb2377",
+    "url": "/static/css/1.98862a8a.chunk.css"
+  },
+  {
+    "revision": "9cf665b08ab6e736e78d277f449fd5be",
+    "url": "/index.html"
+  }
+];
